026f5bb0a38ea4a696691d1fe4a711de4a83ced0
jimmy <vphu456@gmail.com>
2022-10-25 23:20:43 +0700

    todo list

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -26,7 +26,11 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2022, 11, 31, 23, 59, 59);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+const futureDate = new Date(tempYear, tempMonth + 2, tempDay + 6, 23, 59, 59);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -34,8 +38,52 @@ const minutes = futureDate.getMinutes();
 
 let month = futureDate.getMonth();
 month = months[month];
-const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
 
 giveaway.innerHTML = `giveaway ends on ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+function getRemaindingTime() {
+  const today = new Date().getTime();
+
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60m
+  // 1d = 24hr
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+  }
+}
+
+// countdown;
+let countdown = setInterval(getRemaindingTime, 1000);
+//set initial values
+getRemaindingTime();
--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,11 +1,92 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.querySelector("#grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
 
+// clear items
+clearBtn.addEventListener("click", clearItems);
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
 
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("value");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+      <div class="btn-container">
+        <button type="button" class="edit-btn">
+          <i class="fas fa-edit"></i>
+        </button>
+        <button type="button" class="delete-btn">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>`;
+
+    list.appendChild(element);
+    // display alert
+    displayAlert("item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // add to localStorage
+    addToLocalStorage(id.value);
+    // set back to default
+    setBackToDefault();
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    displayAlert("enter value", "danger");
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.innerHTML = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(() => {
+    alert.innerHTML = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+// clear items
+function clearItems() {
+  const items = document.querySelectorAll(".grocery-item");
+  if (items.length > 0) {
+    items.forEach((item) => {
+      list.removeChild(item);
+    });
+  }
+}
+
+// set back to default
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.innerHTML = "submit";
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {}
 // ****** SETUP ITEMS **********
